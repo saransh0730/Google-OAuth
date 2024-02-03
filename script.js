4de895d80bca726f3d12a2109d2e55447a7b583f
Saransh Parashar <saransh.parashar02@gmail.com>
2024-02-03 16:15:44 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function SignIn(){
+    let oauthEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+    let form = document.createElement('form')
+    form.setAttribute('method','GET')
+    form.setAttribute('action',oauthEndpoint)
+
+    var params = {
+        "client_id": "746295690564-6f6dj509esnpf24ha210pui4sgbfngoo.apps.googleusercontent.com",
+        "redirect_uri":"http://127.0.0.1:5500/profile.html",
+        "response_type":"token",
+        "scope":"https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly",
+        "include_granted_scopes": 'true',
+        'state': 'pass-through-value'
+    }
+
+    for(var p in params){
+        let input = document.createElement('input')
+        input.setAttribute('type', 'hidden')
+        input.setAttribute('name',p)
+        input.setAttribute('value', params[p])
+        form.appendChild(input)
+    }
+    document.body.appendChild(form)
+    form.submit()
+}
