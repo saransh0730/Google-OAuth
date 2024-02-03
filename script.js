050284fa745b211334af61e587b787af3a7f5957
Saransh Parashar <saransh.parashar02@gmail.com>
2024-02-03 17:17:50 +0530

    Adding facebook auth

--- script.js
+++ script.js
@@ -25,3 +25,32 @@ function SignIn(){
     document.body.appendChild(form)
     form.submit()
 }
+
+function SignUpWithFacebook() {
+    let oauthEndpoint = "https://www.facebook.com/v12.0/dialog/oauth";
+
+    // Create a form element
+    let form = document.createElement('form');
+    form.setAttribute('method', 'GET');
+    form.setAttribute('action', oauthEndpoint);
+
+    var params = {
+        "client_id": "1145388830231784",
+        "redirect_uri": "http://127.0.0.1:5500/profile.html",
+        "response_type": "token",
+        "scope": "email",
+        "include_granted_scopes": 'true',
+        "state": "pass-through-value"
+    };
+
+    for (var p in params) {
+        let input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
